cdfa1f18c469dba76227ab4586ee2b50f2c9f92c
skaimarvas <skaimarvas@gmail.com>
2024-03-22 18:01:31 +0300

    search bar was created and added on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { useForm } from "react-hook-form";
 import "./App.css";
 import Section from "./components/Section";
 
 const App: React.FC = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
   return (
     <>
       <div className="flex flex-col justify-center items-center gap-5 pt-10">
         <h2 className="text-2xl font-bold text-center">
           Rick and Morty Universe
         </h2>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="w-[400px]">
+          <div>
+            <input
+              {...register("name")}
+              type="text"
+              className="border border-gray-400 px-2 w-full rounded-md"
+            />
+          </div>
+        </form>
 
         <Section />
       </div>
