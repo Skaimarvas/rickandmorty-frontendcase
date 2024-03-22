afa343d427b9a737061d0a169af3cb3f22d43862
skaimarvas <skaimarvas@gmail.com>
2024-03-22 17:06:28 +0300

    title was added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import Option from "./components/Option";
 const App: React.FC = () => {
   return (
     <>
-      <div>
+      <div className="flex flex-col justify-center">
+        <h2 className="text-2xl font-bold text-center">
+          Rick and Morty Universe
+        </h2>
         <Option />
       </div>
     </>
