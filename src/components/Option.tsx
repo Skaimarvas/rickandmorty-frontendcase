f1304479e38a0f6df19c64b267bf82e517faf127
skaimarvas <skaimarvas@gmail.com>
2024-03-22 17:37:52 +0300

    props were added

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,18 +1,31 @@
 import React from "react";
 import rick from "../assets/rm.png";
 
-const Option: React.FC = () => {
+interface OptionProp {
+  img: string;
+  name: string;
+  episodeCount: number;
+}
+
+const Option: React.FC<OptionProp> = ({ img, name, episodeCount }) => {
   return (
-    <div className="flex justify-start gap-5 px-2 border-b border-gray-300 ">
+    <div className="flex justify-start gap-5 px-2 border-b border-gray-300 w-full">
       <div className="flex justify-center items-center">
         <input type="checkbox" />
       </div>
       <div>
-        <img src={rick} alt="" className="w-12 h-12 rounded-md" />
+        <img
+          src={img ? `${img}` : rick}
+          alt=""
+          className="w-12 h-12 rounded-md"
+        />
       </div>
       <div className="flex flex-col">
-        <span>Rick Sanchez</span>
-        <span className="text-sm"> 51 Episodes </span>
+        <span> {name ? `${name}` : `Rick Sanchez`}</span>
+        <span className="text-sm">
+          {" "}
+          {episodeCount ? `${episodeCount}` : `51`} Episodes{" "}
+        </span>
       </div>
     </div>
   );
