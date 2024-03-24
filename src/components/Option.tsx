3947c82c6784c2b71b381c3390d4e273fd30415c
skaimarvas <skaimarvas@gmail.com>
2024-03-24 15:36:02 +0300

    uncheckedChar and checkedChar actions were added on Option

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,18 +1,39 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useAppDispatch } from "../hooks/hook";
+import { checkedChar, uncheckedChar } from "../store/actions/charActions";
 
 interface OptionProp {
   character: any;
 }
 
 const Option: React.FC<OptionProp> = ({ character }) => {
-  const { image, name, episode } = character;
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm();
+  const { image, name, episode, checked, id } = character;
+
+  const handleChecked = () => {
+    if (checked) {
+      dispatch(uncheckedChar(id));
+    } else {
+      dispatch(checkedChar(id));
+    }
+  };
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
 
   return (
-    <form className="w-full">
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full">
       <label className="flex justify-start gap-5 px-2 border-b border-gray-300 container">
         <div className="flex justify-center items-center">
-          <input type="checkbox" />
-          <span className="checkmark"></span>
+          <input
+            {...register("checked")}
+            type="checkbox"
+            checked={checked}
+            onClick={() => handleChecked()}
+          />
         </div>
         <div>
           <img src={image} alt="" className="w-12 h-12 rounded-md" />
