9aba685f733623038020d15b1f363c9fbb11095d
skaimarvas <skaimarvas@gmail.com>
2024-03-29 20:50:35 +0300

    PREV,NEXT,PAGES,SET_FETCH_STATES, FETCH_STATES actions were added

--- src/store/actions/charActions.tsx
+++ src/store/actions/charActions.tsx
@@ -2,11 +2,49 @@ export const CHECKED_CHAR = "CHECKED_CHAR";
 export const ADD_LIST = "ADD_LIST";
 export const UNCHECKED_CHAR = "UNCHECKED_CHAR";
 export const SEARCH_CHAR = "SEARCH_CAR";
+export const PREV = "PREV";
+export const NEXT = "NEXT";
+export const PAGES = "PAGES";
+export const SET_FETCH_STATES = "SET_FETCH_STATES";
+
+export const FETCH_STATES = {
+  notFetched: "NOT_FETCHED",
+  fetching: "FETCHING",
+  fetched: "FETCHED",
+  failed: "FAILED",
+};
+
+export const setFetchState = (fetchState: string) => {
+  return {
+    type: SET_FETCH_STATES,
+    payload: fetchState,
+  };
+};
 
 export const checkedChar = (id: number) => {
   return {
     type: CHECKED_CHAR,
     payload: id,
+  };
+};
+
+export const addPrev = (prev: string) => {
+  return {
+    type: PREV,
+    payload: prev,
+  };
+};
+export const addNext = (next: string) => {
+  return {
+    type: NEXT,
+    payload: next,
+  };
+};
+
+export const pages = (pages: number) => {
+  return {
+    type: PAGES,
+    payload: pages,
   };
 };
 
