eb645b96d0400f31a05b55b983641eb5de1ab3d0
skaimarvas <skaimarvas@gmail.com>
2024-03-29 20:56:47 +0300

    addNext,addPrev,pages and setFetchState were added.

--- src/store/thunks/charThunk.tsx
+++ src/store/thunks/charThunk.tsx
@@ -2,28 +2,49 @@ import axios from "axios";
 
 import { Dispatch } from "redux";
 
-import { addList } from "../actions/charActions";
+import {
+  FETCH_STATES,
+  addList,
+  addNext,
+  addPrev,
+  pages,
+  setFetchState,
+} from "../actions/charActions";
 
-export const addListData = () => {
+export const addListData = (url: string) => {
   return (dispatch: Dispatch) => {
+    dispatch(setFetchState(FETCH_STATES.fetching));
     axios
-      .get("https://rickandmortyapi.com/api/character")
+      .get(url)
       .then((res: any) => {
         console.log(res.data);
         dispatch(addList(res.data.results));
+        dispatch(addNext(res.data.info.next));
+        dispatch(addPrev(res.data.info.prev));
+        dispatch(pages(res.data.info.pages));
+        dispatch(setFetchState(FETCH_STATES.fetched));
       })
-      .catch((err: any) => console.log(err));
+      .catch((err: any) => {
+        console.log(err);
+        dispatch(setFetchState(FETCH_STATES.failed));
+      });
   };
 };
 
 export const searchCharList = (name: string) => {
   return (dispatch: Dispatch) => {
+    dispatch(setFetchState(FETCH_STATES.fetching));
     axios
       .get(`https://rickandmortyapi.com/api/character/?name=${name}`)
       .then((res: any) => {
         console.log(res.data);
         dispatch(addList(res.data.results));
+        dispatch(addNext(res.data.info.next));
+        dispatch(addPrev(res.data.info.prev));
+        dispatch(pages(res.data.info.pages));
+        dispatch(setFetchState(FETCH_STATES.fetched));
       })
       .catch((err: any) => console.log(err));
+    dispatch(setFetchState(FETCH_STATES.failed));
   };
 };
