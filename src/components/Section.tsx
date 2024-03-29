c6f8ca5d0dcad45776f21d8a6a68f2108b924b38
skaimarvas <skaimarvas@gmail.com>
2024-03-29 20:49:15 +0300

    characters name was changed to characterList

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,16 +5,17 @@ import { useAppDispatch, useAppSelector } from "../hooks/hook";
 import { addListData } from "../store/thunks/charThunk";
 
 const Section: React.FC = () => {
-  const { characters } = useAppSelector((store) => store.characters);
+  const { charactersList } = useAppSelector((store) => store);
   const dispatch = useAppDispatch();
+  const initialUrl = "https://rickandmortyapi.com/api/character";
   useEffect(() => {
-    dispatch(addListData());
+    dispatch(addListData(initialUrl));
   }, []);
 
   return (
     <div className="flex flex-col  items-center rounded-md border border-gray-400 w-[400px] h-[500px] overflow-scroll ">
-      {characters &&
-        characters.map((char: any) => (
+      {charactersList &&
+        charactersList.map((char: any) => (
           <Option key={char.id} character={char} />
         ))}
     </div>
