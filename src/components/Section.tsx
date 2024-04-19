f0d0bfeec8d167b2f6a8042662d713470d4637c0
skaimarvas <skaimarvas@gmail.com>
2024-04-19 20:40:09 +0300

    remove useState

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Option from "./Option";
 
 import { useAppDispatch, useAppSelector } from "../hooks/hook";
