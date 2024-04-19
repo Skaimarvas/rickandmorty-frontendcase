6b89bbb9296d72e9b5054cc98565bd1d8d5225e1
skaimarvas <skaimarvas@gmail.com>
2024-04-19 20:31:17 +0300

    add inital url via store

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Option from "./Option";
 
 import { useAppDispatch, useAppSelector } from "../hooks/hook";
@@ -9,23 +9,26 @@ interface OptionProp {
 }
 
 const Section: React.FC<OptionProp> = ({ boldifyMatchingLetters }) => {
-  const { charactersList } = useAppSelector((store) => store);
+  const { charactersList, initialUrl, errorState } = useAppSelector(
+    (store) => store
+  );
   const dispatch = useAppDispatch();
-  const initialUrl = "https://rickandmortyapi.com/api/character";
+
   useEffect(() => {
     dispatch(addListData(initialUrl));
   }, []);
 
   return (
     <div className="flex flex-col  items-center rounded-md border border-gray-400 w-[400px] h-[500px] overflow-scroll ">
-      {charactersList &&
-        charactersList.map((char: any) => (
-          <Option
-            key={char.id}
-            character={char}
-            boldifyMatchingLetters={boldifyMatchingLetters}
-          />
-        ))}
+      {errorState ||
+        (charactersList &&
+          charactersList.map((char: any) => (
+            <Option
+              key={char.id}
+              character={char}
+              boldifyMatchingLetters={boldifyMatchingLetters}
+            />
+          )))}
     </div>
   );
 };
